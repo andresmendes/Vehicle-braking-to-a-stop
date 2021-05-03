5b2a0c69c9e2b8094d899b9b732914d47e72fffa
André de Souza Mendes <asmendes@fei.edu.br>
2021-05-03 10:39:40 -0300

    Update vehicle_braking.m

--- vehicle_braking.m
+++ vehicle_braking.m
@@ -1,6 +1,6 @@
 %% Vehicle braking to a stop
-% Animation of a road vehicle braking to a stop with position, speed,
-% acceleration and braking force plots.
+% Simulation and animation of a road vehicle braking to a stop with
+% position, speed, acceleration and braking force plots.
 %
 %% 
 
